67b60295d2353bf36e76048d4026f36ef13aa19a
vasantymarshalgmail.com <vasantymarshal@gamil.com>
2024-11-06 13:58:16 +0100

    task4

---
+++ task4.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+URL = "https://grocerymate.masterschool.com"
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    yield driver
+    driver.quit()
+
+
+def test_registration(driver):
+    time.sleep(5)
+    create_account_link = driver.find_element(By.XPATH, "//a[@class='switch-link']")
+    create_account_link.click()
+    time.sleep(5)
+
+    full_name_input = driver.find_element(By.XPATH, "//input[@type='text']")
+    email_address_input = driver.find_element(By.XPATH, "//input[@type='email']")
+    password_input = driver.find_element(By.XPATH, "//input[@type='password']")
+    full_name_input.send_keys("Alan Kanth")
+    email_address_input.send_keys("ak@example.com")
+    password_input.send_keys("password123")
+
+    submit_btn = driver.find_element(By.XPATH, "//button[@type='submit']")
+    submit_btn.click()
+    time.sleep(2)
+
+    alert_div = driver.find_element(By.XPATH, "//div[@role='status']")
+    assert alert_div.is_displayed() and alert_div.text
+
+
+if __name__ == "__main__":
+    pytest.main()
